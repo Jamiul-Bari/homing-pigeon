3536d2e4b03db627a27a48d5ecb193fe0bb1e5f2
Jamiul Bari <jamiul.bari98@gmail.com>
2023-07-10 13:23:48 +0600

    chore: broadcasting settings

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatMessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
+    // return view('welcome');
 });
+
+Route::post( 'chat-message', [ChatMessageController::class, 'index'] );
